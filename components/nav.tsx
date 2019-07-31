b9b0dd57521e5e081b5e688dc70325d18aa64bdd
gyarasu <dev.yoshitsugu@gmail.com>
2019-07-31 10:59:49 +0900

    refactor: apply typescript

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
+  { href: 'https://zeit.co/now', label: 'ZEIT', key: null },
+  { href: 'https://github.com/zeit/next.js', label: 'GitHub', key: null }
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
   return link
