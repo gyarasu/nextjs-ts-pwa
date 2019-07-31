93a49772b060b6c4e377684def5c7f62b53a3525
gyarasu <dev.yoshitsugu@gmail.com>
2019-07-31 18:05:51 +0900

    feat: add nuxt custom server

---
+++ server/index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from 'express'
+import next from 'next'
+import { join } from 'path'
+
+const dev = process.env.NODE_ENV !== 'production'
+const PORT = process.env.PORT || 3000
+
+const app = next({ dir: '.', dev })
+const handle = app.getRequestHandler()
+
+const main = async () => {
+  await app.prepare()
+  const server = express()
+
+  // service-worker
+  server.get('/service-worker.js', (req: Request, res: Response) => {
+    const filePath = join(__dirname, '../../.next/service-worker.js')
+    return res.sendFile(filePath)
+  })
+
+  // nextjs routing
+  server.get('*', (req, res) => handle(req, res))
+
+  server.listen(PORT, () => {
+    console.log(`> Ready on http://localhost:${PORT}`)
+  })
+}
+
+main()
