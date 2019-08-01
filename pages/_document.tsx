6ebae08be7674c0656d12e632efb8edaeb64005f
gyarasu <dev.yoshitsugu@gmail.com>
2019-08-01 10:24:13 +0900

    feat: add manifest file to enable A2HS

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ export default class extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
+          <link rel="manifest" href="/static/manifest.json" />
         </Head>
         <body>
           <Main />
