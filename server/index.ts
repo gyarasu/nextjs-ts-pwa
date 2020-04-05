b908ad941bf91ff383ba5c7f974b72e7831920ec
gyarasu <dev.yoshitsugu@gmail.com>
2020-04-05 14:05:00 +0900

    fix: yarn lint

--- server/index.ts
+++ server/index.ts
@@ -1,29 +1,29 @@
-import express, { Request, Response } from 'express'
-import next from 'next'
-import { join } from 'path'
+import express, { Request, Response } from 'express';
+import next from 'next';
+import { join } from 'path';
 
-const dev = process.env.NODE_ENV !== 'production'
-const PORT = process.env.PORT || 3000
+const dev = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 3000;
 
-const app = next({ dir: '.', dev })
-const handle = app.getRequestHandler()
+const app = next({ dir: '.', dev });
+const handle = app.getRequestHandler();
 
-const main = async () => {
-  await app.prepare()
-  const server = express()
+const main = async (): Promise<void> => {
+  await app.prepare();
+  const server = express();
 
   // service-worker
   server.get('/service-worker.js', (req: Request, res: Response) => {
-    const filePath = join(__dirname, '../../.next/service-worker.js')
-    return res.sendFile(filePath)
-  })
+    const filePath = join(__dirname, '../../.next/service-worker.js');
+    return res.sendFile(filePath);
+  });
 
   // nextjs routing
-  server.get('*', (req, res) => handle(req, res))
+  server.get('*', (req, res) => handle(req, res));
 
   server.listen(PORT, () => {
-    console.log(`> Ready on http://localhost:${PORT}`)
-  })
-}
+    console.log(`> Ready on http://localhost:${PORT}`);
+  });
+};
 
-main()
+main();
